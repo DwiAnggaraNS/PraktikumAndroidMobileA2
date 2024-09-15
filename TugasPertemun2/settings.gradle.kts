2f373b99ef3be67938a65413499f3e4256fffabf
Dwi Anggara Najwan Sugama <dwianggarans@gmail.com>
2024-09-15 19:03:30 +0700

    add project tugas pertemuan 2

---
+++ TugasPertemun2/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TugasPertemun2"
+include(":app")
+ 
